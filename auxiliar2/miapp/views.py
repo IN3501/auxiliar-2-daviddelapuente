dd43f5cd0b915c99358fdf6d32d2ed12dcf19d2a
daviddelapuente <daviddelapuentesalmon@gmail.com>
2019-08-13 22:22:16 -0400

    crea la primera pagina

---
+++ auxiliar2/miapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'miapp/index.html')
+
+def pestaña(request):
+	return render(request, 'miapp/pestaña.html')
+
+def aux(request):
+	return render(request, 'miapp/aux.html')
